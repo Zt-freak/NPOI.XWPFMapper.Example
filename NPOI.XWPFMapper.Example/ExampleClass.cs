8fc32c98010cbc08ee1cfac37fdac41bf2f0bddc
Apodemus Erectus <bnkho98@hotmail.com>
2022-09-17 15:55:07 +0200

    inital commit

---
+++ NPOI.XWPFMapper.Example/ExampleClass.cs
@@ -0,0 +1,42 @@
+﻿using NPOI.XWPFMapper.Attributes;
+using NPOI.XWPFMapper.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NPOI.XWPFMapper.Example
+{
+    internal class ExampleClass : IXWPFMappable // Implement the IXWPFMappable interface to make a class mappable
+    {
+        // Add the XWPFPropertyattribute to make a member mappable.
+        [XWPFProperty("Color")]
+        public ExampleEnum Enum { get; set; }
+
+        [XWPFProperty("Name")]
+        public string? Name { get; set; }
+
+        // This child class also implements IXWPFMappable and will become a nested table.
+        [XWPFProperty("Address")]
+        public ExampleChildClass? Address { get; set; }
+
+        // Without the XWPFPropertyAttribute a member will be ignored by the mapping.
+        public string? IgnoredMember { get; set; }
+    }
+
+    internal class ExampleChildClass : IXWPFMappable
+    {
+        [XWPFProperty("Street")]
+        public string? Address { get; set; }
+        [XWPFProperty("Place")]
+        public string? City { get; set; }
+        [XWPFProperty("Country")]
+        public string? CountryCode { get; set; }
+    }
+
+    internal enum ExampleEnum
+    {
+        Red, Blue, Green
+    }
+}
---
+++ NPOI.XWPFMapper.Example/Program.cs
@@ -0,0 +1,28 @@
+﻿using NPOI.XWPF.UserModel;
+using NPOI.XWPFMapper.Enums;
+using NPOI.XWPFMapper.Example;
+using NPOI.XWPFMapper.Managers;
+
+XWPFDocument document = new XWPFDocument();
+
+ExampleClass exampleData = new ExampleClass()
+{
+    Enum = ExampleEnum.Red,
+    Name = "This is a test",
+    IgnoredMember = "This member will be ignored",
+    Address = new ExampleChildClass()
+    {
+        Address = "Burgemeester Schönfeldplein",
+        City = "Winschoten",
+        CountryCode = "NL"
+    }
+};
+
+// XWPFTableWrapper requires a Type argument and an XWPFDocument object to work
+// In addition you can optionally set the direction of the table with an enum XWPFTableAlignment (default is Row)
+
+XWPFTableWrapper<ExampleClass> wrapper = new XWPFTableWrapper<ExampleClass>(document, XWPFTableAlignment.Column);
+wrapper.Insert(exampleData);
+
+using FileStream fs = new FileStream("test.docx", FileMode.Create);
+document.Write(fs);
